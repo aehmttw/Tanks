311a2748f276b060d62c6fd4b03ff12eea1b0b55
Hallowizer <40325350+Hallowizer@users.noreply.github.com>
2019-08-25 20:43:01 -0700

    Get all the native library **** to work

---
+++ src/test/java/tanks/test/TanksTest.java
@@ -0,0 +1,11 @@
+package tanks.test;
+
+import lombok.experimental.UtilityClass;
+import tanks.Game;
+
+@UtilityClass
+public class TanksTest {
+	public void main(String[] args) {
+		Game.main(args);
+	}
+}

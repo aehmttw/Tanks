d76dda056d7f47a4be87d34cdda49592391b48ac
pythoncoder1234 <fdsnail2@gmail.com>
2023-01-11 12:26:39 -0800

    Implement pause on lost focus and caps lock behavior

--- src/main/java/tanks/gui/screen/ScreenGame.java
+++ src/main/java/tanks/gui/screen/ScreenGame.java
@@ -2592,6 +2592,13 @@ public class ScreenGame extends Screen implements IHiddenChatboxScreen, IPartyGa
 			paused = true;
 	}
 
+	@Override
+	public void onFocusChange(boolean focused)
+	{
+		if (!focused && ((Game.autostart && !cancelCountdown) || playing) && !npcShopScreen)
+			paused = true;
+	}
+
 	public void saveRemainingTanks()
 	{
 		if (!savedRemainingTanks && Crusade.crusadeMode && Crusade.currentCrusade != null)

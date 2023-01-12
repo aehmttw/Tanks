f048cb8512f7db6e1efefe8206c98605ef8c3c35
pythoncoder1234 <fdsnail2@gmail.com>
2023-01-12 11:09:02 -0800

    Fix bugs and modify options layout

--- src/main/java/tanks/gui/screen/ScreenCrusadeDetails.java
+++ src/main/java/tanks/gui/screen/ScreenCrusadeDetails.java
@@ -139,7 +139,8 @@ public class ScreenCrusadeDetails extends Screen
         {
             if (!c.started)
             {
-                begin.posY -= this.objYSpace;
+                begin.posY -= this.objYSpace * 2;
+                edit.posY -= this.objYSpace;
                 delete.posY -= this.objYSpace;
                 back.posY -= this.objYSpace;
             }

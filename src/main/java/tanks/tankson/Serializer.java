73cbfc168251bedf8889881ff4b55beba61bc888
Parth Iyer <parth.iyer@gmail.com>
2024-09-17 20:31:46 -0400

    It kinda ish works

--- src/main/java/tanks/tankson/Serializer.java
+++ src/main/java/tanks/tankson/Serializer.java
@@ -2,6 +2,9 @@ package tanks.tankson;
 
 import basewindow.Model;
 import com.google.gson.Gson;
+import tanks.bullet.Bullet;
+import tanks.tank.Explosion;
+import tanks.tank.TankAIControlled;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -23,7 +26,7 @@ public final class Serializer {
     public static Map<String, Object> toMap(Object o) {
         if (isTanksONable(o)) {
             HashMap<String, Object> p = new HashMap<>();
-            p.put("type", o.getClass().getName());
+            p.put("obj_type", o.getClass().getName());
             for (Field f : o.getClass().getFields()){
                 if (f.isAnnotationPresent(Property.class)) {
                     try {
@@ -42,8 +45,8 @@ public final class Serializer {
                             }
                         } else if (o2 instanceof Enum) {
                             p.put(f.getName(), ((Enum) o2).name());
-                        } else if (o2 instanceof Model) {
-                            p.put(f.getName(), o2.toString());
+                        } else if (o2 instanceof Serializable) {
+                            p.put(f.getName(), ((Serializable) o2).serialize());
                         } else {
                             p.put(f.getName(), f.get(o));
                         }
@@ -58,5 +61,50 @@ public final class Serializer {
 
     public static String toJson(Object o) {
         return gson.toJsonTree(toMap(o)).toString();
+    }
+
+    public static Object fromJson(String s) {
+        return parseObject(gson.fromJson(s,Map.class));
+    }
+
+    public static Object parseObject(Map m) {
+        Object o;
+        if (m.get("obj_type").equals("tanks.tank.TankAIControlled")) {
+            o = (Object) new TankAIControlled("", 0, 0, 0, 0, 0, 0, 0, TankAIControlled.ShootAI.none);
+        } else if (m.get("obj_type").equals("tanks.tank.Explosion")) {
+            o = (Object) new Explosion();
+        } else if (m.get("obj_type").equals("")) {
+            o = (Object) new Bullet();
+        } else {
+            o = null;
+        }
+        for (Field f : TankAIControlled.class.getFields()){
+            if (f.isAnnotationPresent(Property.class)) {
+                try {
+                    Object o2 = f.get(o);
+                    if (isTanksONable(o2)) {
+                        f.set(o2, parseObject((Map) m.get(f.getName())));
+                    } else if (o2 instanceof ArrayList) {
+                        if (!((ArrayList) o2).isEmpty() && isTanksONable(((ArrayList) o2).get(0))){
+                            ArrayList o3s = new ArrayList();
+                            for (Map o3 : ((ArrayList<Map>) m.get(f.getName()))) {
+                                o3s.add(parseObject(o3));
+                            }
+                            f.set(o2,o3s);
+                        } else {
+                            f.set(o2,m.get(f.getName()));
+                        }
+                    } else if (o2 instanceof Enum) {
+                        f.set(o2,Enum.valueOf((Class<? extends Enum>) f.getType(), (String) m.get(f.getName())));
+                    } else if (o2 instanceof Serializable) {
+                        f.set(o2,((Serializable) o2).deserialize((String) m.get(f.getName())));
+                    } else {
+                        f.set(o2,m.get(f.getName()));
+                    }
+                } catch (Exception ignore) {
+                }
+            }
+        }
+        return o;
     }
 }

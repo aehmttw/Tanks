08b146852b3500f29552d327a2a30b5772232fc4
pythoncoder1234 <fdsnail2@gmail.com>
2025-02-05 10:04:31 -0800

    general cases table
    Signed-off-by: pythoncoder1234 <fdsnail2@gmail.com>

--- src/main/java/tanks/tankson/Compatibility.java
+++ src/main/java/tanks/tankson/Compatibility.java
@@ -2,28 +2,46 @@ package tanks.tankson;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Compatibility {
     /** Add a static initializer to initialize compatibility table if necessary */
     public static final HashMap<String, Function<Object, Object>> compatibility_table = new HashMap<>();
+    public static final HashMap<Class<?>, BiFunction<Field, Object, Object>> general_table = new HashMap<>();
     public static final HashMap<String, String> field_table = new HashMap<>();
 
-    public static Object convert(Field f, Object o) {
-        try
-        {
-            if (o instanceof Boolean)
-                return ((Boolean) o) ? f.getType().getConstructor().newInstance() : null;
-        }
-        catch (Exception ignored)
-        {
+    static
+    {
+        addGeneralCase(Boolean.class, Compatibility::applyBoolean);
+    }
 
-        }
+    public static <V> void addGeneralCase(Class<V> cls, BiFunction<Field, V, Object> func)
+    {
+        //noinspection unchecked
+        general_table.put(cls, (BiFunction<Field, Object, Object>) func);
+    }
+
+    public static Object convert(Field f, Object o) {
+        if (general_table.containsKey(o.getClass()))
+            return general_table.get(o.getClass()).apply(f, o);
 
         return compatibility_table.get(Serializer.getId(f)).apply(o);
     }
 
     public static String convert(String f) {
         return field_table.get(f);
+    }
+
+    private static Object applyBoolean(Field f, Boolean b)
+    {
+        try
+        {
+            return b ? f.getType().getConstructor().newInstance() : null;
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 }
